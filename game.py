2f5e2fd989fe21c56694b4a716c43e41622943e5
Chris Sulz <Chris.Sulz@xtelinternational.com>
2015-06-18 14:46:02 -0600

    Add new gears with r key

--- game.py
+++ game.py
@@ -32,6 +32,8 @@ def main():
             if event.type == pygame.KEYUP:
                 if event.dict['key'] in [pygame.K_q,pygame.K_ESCAPE,pygame.K_BREAK]:
                     sys.exit()
+                if event.dict['key'] == pygame.K_r:
+                    gears.append(Gear(bound,pygame.Color(random.randint(0,255),random.randint(0,255),random.randint(0,255))))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 #find gear mouse is over
                 selGear = gears[mouse.collidelist([g.rect for g in gears])]
