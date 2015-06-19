51f46c7f408f82a238efff858a88640663e83e17
Chris Sulz <Chris.Sulz@xtelinternational.com>
2015-06-19 16:28:01 -0600

    Fix size of gears, should have diameter of 2*radius

--- gear.py
+++ gear.py
@@ -2,35 +2,34 @@ import pygame
 import math
 import os
 class Gear:
-    def __init__(self, bound, color=pygame.Color("white"), teeth=16, mass=10):
+    def __init__(self, bound, fixed=False, color=pygame.Color("white"), teeth=12, mass=10):
+        self.fixed = False
         self.color = color
         self.t = teeth
         self.m = mass
-        self.w = 10
+        self.w = 0
         self.degree = 0
         
-        p = 2 * self.t
-        self.r = int((p * self.t) / (2 * math.pi))
+        self.r = int((2*self.t * self.t) / (2 * math.pi))
         
         # create image surface
-        self.image = pygame.Surface((self.r,self.r))
+        self.image = pygame.Surface((2*self.r,2*self.r))
         self.image.set_colorkey(pygame.Color("magenta"))
         self.image.fill(pygame.Color("magenta"))
 
         gearsurf = pygame.image.load(os.path.join(os.getcwd(), 'images', 'run.png'))
-        gearsurf = pygame.transform.scale(gearsurf, (self.r,self.r))
+        gearsurf = pygame.transform.scale(gearsurf, (2*self.r,2*self.r))
         self.rect = gearsurf.get_rect()
-        self.rect.center = self.image.get_rect().center
         
         self.image.blit(gearsurf, self.rect)
         self.rotimage = self.image
         
         # create rectangles
-        self.pos = pygame.Rect(0, 0, self.r, self.r)
+        self.pos = pygame.Rect(0, 0, 2*self.r, 2*self.r)
 
         # move gear
         self.setPosition(bound.center[0],bound.center[1])
-
+        
     def draw(self, screen):
         screen.blit(self.rotimage, self.rect)
         
@@ -41,8 +40,8 @@ class Gear:
         self.rect.center = oldrect.center
         self.degree += self.w * dt/1000.0
         
-        
     def setPosition(self,newx,newy):
         self.pos.center = newx,newy
         self.rect.center = newx,newy
         self.x, self.y = self.pos.center
+        
