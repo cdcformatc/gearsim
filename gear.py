42f966e9bdf5458adce608dc24fed02c85b4e0a2
Chris Sulz <Chris.Sulz@xtelinternational.com>
2015-06-19 10:47:05 -0600

    Load a gear image

--- gear.py
+++ gear.py
@@ -1,32 +1,33 @@
 import pygame
 import math
+import os
 class Gear:
     def __init__(self, bound, color=pygame.Color("white"), teeth=16, mass=10):
         self.color = color
         self.t = teeth
         self.m = mass
         self.w = .1
+        self.degree = 0
         
         p = 2 * self.t
         self.r = int((p * self.t) / (2 * math.pi))
         
         # create image surface
-        self.image = pygame.Surface((self.r*2,self.r*2))
+        self.image = pygame.Surface((self.r,self.r))
         #self.image.set_colorkey(pygame.Color("magenta"))
         self.image.fill(pygame.Color("magenta"))
-        self.rect = self.image.get_rect()
+
+        gearsurf = pygame.image.load(os.path.join(os.getcwd(), 'images', 'run.png'))
+        gearsurf = pygame.transform.scale(gearsurf, (self.r,self.r))
+        self.rect = gearsurf.get_rect()
+        self.rect.center = self.image.get_rect().center
         
-        self.degree = 0
+        self.image.blit(gearsurf, self.rect)
+        self.rotimage = self.image
         
         # create rectangles
         self.pos = pygame.Rect(0, 0, self.r, self.r)
-        toDraw = pygame.Rect(0,0, self.r, self.r)
-        toDraw.center = self.rect.center
-        
-        # draw onto the image
-        pygame.draw.rect(self.image, self.color, toDraw)
-        self.rotimage = self.image
-        
+
         # move gear
         self.setPosition(bound.center[0],bound.center[1])
 
